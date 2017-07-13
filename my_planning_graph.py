0c41dbb7ae29df736c425e35de380324369fc5a9
sethbunke <sethbunke@hotmail.com>
2017-07-13 16:21:00 -0700

    my planning search - 3 test failing

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -473,6 +473,17 @@ class PlanningGraph():
         :return: bool
         """
         # TODO test for negation between nodes
+        # return False
+
+        # if node_s1.is_mutex(node_s2) or node_s2.is_mutex(node_s1):
+        #     return True
+        # else:
+        #     return False
+
+        #same symbol but different one is pos and the other is negative
+        if (node_s1.symbol == node_s2.symbol) and (node_s1.is_pos != node_s2.is_pos):
+            return True
+
         return False
 
     def inconsistent_support_mutex(self, node_s1: PgNode_s, node_s2: PgNode_s):
